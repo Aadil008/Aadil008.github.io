f0cc251b68bbb2b3aa26cf01a79dd3317de262b5
Aadil008 <118273276+Aadil008@users.noreply.github.com>
2025-01-02 10:27:02 +0530

    Add files via upload

---
+++ content_option.js
@@ -0,0 +1,158 @@
+const logotext = "PORTFOLIO";
+const meta = {
+    title: "SALEH AADIL",
+    description: "I'm SALEH AADIL, a technologist and cybersecurity enthuist.",
+};
+
+const introdata = {
+    title: "Hi, I'm SALEH AADIL",
+    animated: {
+        first: "Technologist",
+        second: "Visionary",
+        third: "Ethical Hacker",
+        fourth: "Programmer",
+        fifth: "Malware Developer",
+        sixth: "Analyst",
+        // add more if you'd like but make sure to update /src/pages/home/index.js Line 29
+    },
+    description: "Dedicated technology enthusiast with an insatiable curiosity for unraveling the complexities of our digital world.",
+    your_img_url: "https://i.ibb.co/wRjcnGL/me.jpg",
+};
+
+const dataabout = {
+    title: "Who am I?",
+    aboutme: "Aspiring Cyber Security Professional aiming to leverage my technical skills and passion for cybersecurity in a dynamic and innovative environment. Seeking to contribute to an organization's security posture by applying knowledge in network security, vulnerability assessments, and incident response. ",
+};
+const worktimeline = [{
+        jobtitle: "Cyber Security Intern",
+        where: "Techdefence",
+        date: "2024",
+    },
+    {
+        jobtitle: "Cyber Security Intern",
+        where: "Prashunet PvtLtd",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "Python",
+    },
+    
+    {
+        name: "SOC",
+    },
+    
+    {
+        name: "Vulnerability Assessment",
+    },
+    
+    {
+        name: "Bash",
+    },
+
+    {
+        name: "Powershell",
+    },
+
+    {
+        name: "Reverse Engineering",
+    },
+
+    {
+        name: "Web-Exploitation",
+    },
+
+    {
+        name: "Network Exploitation",
+    },           
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "Aadilsaleh1210@gmail.com",
+    description: "Looking forward to hearing from you!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "SERVICE_ID",
+    YOUR_TEMPLATE_ID: "TEMPLATE_ID",
+    YOUR_USER_ID: "USER_ID (PUBLIC_KEY)",
+};
+
+const socialprofils = {
+    github: "https://github.com/Aadil008/",
+    linkedin: "https://www.linkedin.com/in/saleh-aadil-6a4968294/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
